a6a2e6e2736d0da3e2b3f0847555279f8e0129fd
박민규 <pmq0101@naver.com>
2024-11-05 19:06:07 +0900

    test(MeetingServiceTest): 테스트 코드 기본 설정

---
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/MeetingServiceTest.java
@@ -0,0 +1,43 @@
+package org.cookieandkakao.babting.domain.meeting.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MeetingServiceTest {
+
+    @Test
+    void createMeeting() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void updateMeeting() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void joinMeeting() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void exitMeeting() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void checkHost() {
+        //given
+        //when
+        //then
+    }
+}
