487f191f1b83e1e901c040709c40a5406a407225
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-11-05 14:50:10 +0900

    refactor(MeetingEventService): 서비스 분리

--- src/main/java/org/cookieandkakao/babting/domain/meeting/service/MeetingEventService.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/service/MeetingEventService.java
@@ -1,7 +1,5 @@
 package org.cookieandkakao.babting.domain.meeting.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -9,26 +7,18 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import org.cookieandkakao.babting.common.exception.customexception.JsonConversionException;
-import org.cookieandkakao.babting.domain.calendar.dto.request.EventCreateRequest;
-import org.cookieandkakao.babting.domain.calendar.dto.request.TimeCreateRequest;
-import org.cookieandkakao.babting.domain.calendar.dto.response.EventCreateResponse;
 import org.cookieandkakao.babting.domain.calendar.dto.response.EventGetResponse;
 import org.cookieandkakao.babting.domain.calendar.dto.response.TimeGetResponse;
-import org.cookieandkakao.babting.domain.calendar.entity.Event;
-import org.cookieandkakao.babting.domain.calendar.repository.EventRepository;
-import org.cookieandkakao.babting.domain.calendar.service.EventService;
-import org.cookieandkakao.babting.domain.calendar.service.TalkCalendarClientService;
 import org.cookieandkakao.babting.domain.calendar.service.TalkCalendarService;
+import org.cookieandkakao.babting.domain.food.repository.FoodRepository;
+import org.cookieandkakao.babting.domain.food.service.FoodService;
 import org.cookieandkakao.babting.domain.meeting.dto.request.ConfirmMeetingGetRequest;
 import org.cookieandkakao.babting.domain.meeting.dto.request.MeetingEventCreateRequest;
 import org.cookieandkakao.babting.domain.meeting.dto.request.MeetingTimeCreateRequest;
 import org.cookieandkakao.babting.domain.meeting.dto.response.TimeAvailableGetResponse;
 import org.cookieandkakao.babting.domain.meeting.entity.Meeting;
 import org.cookieandkakao.babting.domain.meeting.entity.MeetingEvent;
-import org.cookieandkakao.babting.domain.meeting.entity.MemberMeeting;
 import org.cookieandkakao.babting.domain.meeting.repository.MeetingEventRepository;
-import org.cookieandkakao.babting.domain.member.entity.KakaoToken;
 import org.cookieandkakao.babting.domain.member.entity.Member;
 import org.cookieandkakao.babting.domain.member.service.MemberService;
 import org.springframework.stereotype.Service;
@@ -39,55 +29,51 @@ public class MeetingEventService {
 
     private final MemberService memberService;
     private final TalkCalendarService talkCalendarService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
     private final MeetingService meetingService;
     private static final String TIME_ZONE = "Asia/Seoul";
     private static final List<Integer> DEFAULT_REMINDER_TIMES = List.of(15, 30);
+    private final MeetingValidationService meetingValidationService;
+    private final MeetingEventCreateService meetingEventCreateService;
     private final MeetingEventRepository meetingEventRepository;
-    private final EventService eventService;
+    private final FoodService foodService;
 
     public MeetingEventService(MemberService memberService,
         TalkCalendarService talkCalendarService,
         MeetingService meetingService,
-        MeetingEventRepository meetingEventRepository, EventService eventService) {
+        MeetingValidationService meetingValidationService,
+        MeetingEventCreateService meetingEventCreateService,
+        MeetingEventRepository meetingEventRepository,
+        FoodService foodService) {
         this.memberService = memberService;
         this.talkCalendarService = talkCalendarService;
         this.meetingService = meetingService;
+        this.meetingValidationService = meetingValidationService;
+        this.meetingEventCreateService = meetingEventCreateService;
         this.meetingEventRepository = meetingEventRepository;
-        this.eventService = eventService;
+        this.foodService = foodService;
     }
 
-    // 모임 확정되면 일정 생성
+    // 모임 확정
     public void confirmMeeting(Long memberId, Long meetingId,
         ConfirmMeetingGetRequest confirmMeetingGetRequest) {
         Member member = memberService.findMember(memberId);
         Meeting meeting = meetingService.findMeeting(meetingId);
-        validateHostPermission(member, meeting);
-        validateMeetingConfirmation(meeting);
+        Long foodId = confirmMeetingGetRequest.confirmFoodId();
+
+        meetingValidationService.validateHostPermission(member, meeting);
+        meetingValidationService.validateMeetingConfirmation(meeting);
 
         meeting.confirmDateTime(confirmMeetingGetRequest.confirmDateTime());
+        meeting.confirmFood(foodService.findByFoodId(foodId));
 
         MeetingTimeCreateRequest meetingTimeCreateRequest = createMeetingTimeRequest(meeting);
         MeetingEventCreateRequest meetingEventCreateRequest = createMeetingEventRequest(meeting,
             meetingTimeCreateRequest);
 
-        List<Long> memberIds = getMemberIdInMeetingId(meetingId);
+        List<Long> memberIds = meetingService.getMemberIdInMeetingId(meetingId);
 
         for (Long currentMemberId : memberIds) {
-            addMeetingEvent(currentMemberId, meetingEventCreateRequest);
-        }
-    }
-
-    private void validateHostPermission(Member member, Meeting meeting) {
-        MemberMeeting memberMeeting = meetingService.findMemberMeeting(member, meeting);
-        if (!memberMeeting.isHost()) {
-            throw new IllegalStateException("권한이 없습니다.");
-        }
-    }
-
-    private void validateMeetingConfirmation(Meeting meeting) {
-        if (meeting.getConfirmDateTime() != null) {
-            throw new IllegalStateException("이미 모임 시간이 확정되었습니다.");
+            meetingEventCreateService.addMeetingEvent(currentMemberId, meetingEventCreateRequest);
         }
     }
 
@@ -105,44 +91,6 @@ public class MeetingEventService {
             DEFAULT_REMINDER_TIMES);
     }
 
-    // 일정 생성 후 캘린더에 일정 추가
-    public EventCreateResponse addMeetingEvent(Long memberId,
-        MeetingEventCreateRequest meetingEventCreateRequest) {
-        EventCreateRequest eventCreateRequest = convertToEventCreateRequest(
-            meetingEventCreateRequest);
-        return talkCalendarService.createEvent(eventCreateRequest, memberId);
-    }
-
-    private EventCreateRequest convertToEventCreateRequest(
-        MeetingEventCreateRequest meetingEventCreateRequest) {
-        return new EventCreateRequest(
-            meetingEventCreateRequest.title(),
-            meetingEventCreateRequest.time().toTimeCreateRequest(), null,
-            meetingEventCreateRequest.reminders(), null);
-    }
-
-    private String getKakaoAccessToken(Long memberId) {
-        KakaoToken kakaoToken = memberService.getKakaoToken(memberId);
-        return kakaoToken.getAccessToken();
-    }
-
-    private String convertToJSONString(MeetingEventCreateRequest eventCreateRequest) {
-        try {
-            return objectMapper.writeValueAsString(eventCreateRequest);
-        } catch (JsonProcessingException e) {
-            throw new JsonConversionException("JSON 변환 중 오류가 발생했습니다.");
-        }
-    }
-
-    private List<Long> getMemberIdInMeetingId(Long meetingId) {
-        Meeting meeting = meetingService.findMeeting(meetingId);
-        List<MemberMeeting> memberMeetings = meetingService.findAllMemberMeeting(meeting);
-
-        return memberMeetings.stream()
-            .map(memberMeeting -> memberMeeting.getMember().getMemberId())
-            .toList();
-    }
-
     /**
      * 빈 시간대 조회 로직 설명
      * <p>
@@ -153,7 +101,7 @@ public class MeetingEventService {
      * mergedTime에 있는 시간들을 순회하면서 i번째 끝 시간 ~ i+1번째 시작 시간으로 시간 생성
      */
     public TimeAvailableGetResponse findAvailableTime(Long meetingId) {
-        List<Long> joinedMemberIds = getMemberIdInMeetingId(meetingId);
+        List<Long> joinedMemberIds = meetingService.getMemberIdInMeetingId(meetingId);
         Meeting meeting = meetingService.findMeeting(meetingId);
         String from = meeting.getStartDate().toString();
         String to = meeting.getEndDate().toString();
@@ -273,39 +221,7 @@ public class MeetingEventService {
         return availableTimes;
     }
 
-
-    // 모임별 개인적으로 피하고 싶은 시간 저장하기
-    @Transactional
-    public void saveMeetingAvoidTime(Long memberId, Long meetingId,
-        List<MeetingTimeCreateRequest> avoidTimeCreateRequests) {
-
-        // 피하고 싶은 시간 없으면 그냥 종료
-        if (avoidTimeCreateRequests.isEmpty() || avoidTimeCreateRequests == null) {
-            return;
-        }
-
-        Member member = memberService.findMember(memberId);
-        Meeting meeting = meetingService.findMeeting(meetingId);
-        MemberMeeting memberMeeting = meetingService.findMemberMeeting(member, meeting);
-
-        for (MeetingTimeCreateRequest avoidTimeCreateRequest : avoidTimeCreateRequests) {
-            createAndSaveMeetingEvent(memberMeeting, avoidTimeCreateRequest);
-        }
-    }
-
-    // 피하고 싶은 시간을 기반으로 MeetingEvent 생성 및 저장
-    private void createAndSaveMeetingEvent(MemberMeeting memberMeeting,
-        MeetingTimeCreateRequest meetingTimeCreateRequest) {
-        // 시간 정보를 Event로 생성 후 저장
-        TimeCreateRequest timeCreateRequest = meetingTimeCreateRequest.toTimeCreateRequest();
-        Event avoidEvent = eventService.saveAvoidTimeEvent(timeCreateRequest.toEntity());
-
-        // MeetingEvent로 저장
-        MeetingEvent meetingEvent = new MeetingEvent(memberMeeting, avoidEvent);
-        saveMeetingEvent(meetingEvent);
-    }
-
-    private void saveMeetingEvent(MeetingEvent meetingEvent) {
+    public void saveMeetingEvent(MeetingEvent meetingEvent) {
         meetingEventRepository.save(meetingEvent);
     }
 }
