9f571c00aae4b22fe16aee81fbe3be0ec7652895
박민규 <pmq0101@naver.com>
2024-11-05 19:49:39 +0900

    test(MeetingServiceTest): 테스트 코드 추가

---
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingCreateTest.java
@@ -0,0 +1,22 @@
+package org.cookieandkakao.babting.domain.meeting.service.meetingservice;
+
+
+import org.cookieandkakao.babting.domain.meeting.service.MeetingService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingCreateTest {
+
+    @Autowired
+    private MeetingService meetingService;
+
+    //모임 생성
+    @Test
+    void 모임_생성() {
+        //given
+        //when
+        //then
+    }
+}
