c60f75375974203b8b6689248bfe017381846fa4
박민규 <pmq0101@naver.com>
2024-11-08 15:57:12 +0900

    feat(exception): 모임 예외 컨트롤러 추가

---
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/exception/MeetingExceptionHandler.java
@@ -0,0 +1,7 @@
+package org.cookieandkakao.babting.domain.meeting.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MeetingExceptionHandler {
+}
