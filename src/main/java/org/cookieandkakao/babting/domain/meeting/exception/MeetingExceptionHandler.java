f5749e2f71802eab4421a6b0fe8ba63b0ef0beda
박민규 <pmq0101@naver.com>
2024-11-08 16:06:34 +0900

    feat(MeetingExceptionHandler): 예외 추가

--- src/main/java/org/cookieandkakao/babting/domain/meeting/exception/MeetingExceptionHandler.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/exception/MeetingExceptionHandler.java
@@ -1,7 +1,23 @@
 package org.cookieandkakao.babting.domain.meeting.exception;
 
+import org.cookieandkakao.babting.common.apiresponse.ApiResponseBody.FailureBody;
+import org.cookieandkakao.babting.common.apiresponse.ApiResponseGenerator;
+import org.cookieandkakao.babting.domain.meeting.exception.meeting.MeetingNotFoundException;
+import org.cookieandkakao.babting.domain.meeting.exception.membermeeting.MemberMeetingNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class MeetingExceptionHandler {
+    @ExceptionHandler(MeetingNotFoundException.class)
+    public ResponseEntity<FailureBody> handleMeetingNotFoundException(MeetingNotFoundException ex) {
+        return ApiResponseGenerator.fail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+
+    @ExceptionHandler(MemberMeetingNotFoundException.class)
+    public ResponseEntity<FailureBody> handleMemberMeetingNotFoundException(MemberMeetingNotFoundException ex) {
+        return ApiResponseGenerator.fail(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
 }
