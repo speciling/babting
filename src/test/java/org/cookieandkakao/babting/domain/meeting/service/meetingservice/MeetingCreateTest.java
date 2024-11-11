d1ebbe27403763bda5a78fb6a3448f20e49c50e3
박민규 <pmq0101@naver.com>
2024-11-12 04:17:14 +0900

    test(MeetingServiceTest, MeetingCreateTest): 기본 모임 생성 메서드 추가

--- src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingCreateTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingCreateTest.java
@@ -3,10 +3,6 @@ package org.cookieandkakao.babting.domain.meeting.service.meetingservice;
 
 import static org.mockito.Mockito.*;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import org.cookieandkakao.babting.domain.meeting.dto.request.LocationCreateRequest;
-import org.cookieandkakao.babting.domain.meeting.dto.request.MeetingCreateRequest;
 import org.cookieandkakao.babting.domain.meeting.entity.Meeting;
 import org.junit.jupiter.api.Test;
 
@@ -16,13 +12,7 @@ class MeetingCreateTest extends MeetingServiceTest{
     @Test
     void 모임_생성_성공() {
         //given
-        LocationCreateRequest baseLocation = new LocationCreateRequest("전대", "11", 1.1, 1.1);
-        MeetingCreateRequest meetingCreateRequest = new MeetingCreateRequest(baseLocation, "밥팅",
-            LocalDate.now().plusDays(1), LocalDate.now().plusDays(2), 3, LocalTime.of(14, 0),
-            LocalTime.of(17, 0));
-
         when(meetingRepository.save(any(Meeting.class))).thenReturn(meetingCreateRequest.toEntity());
-
         //when
         meetingService.createMeeting(1L, meetingCreateRequest);
         //then
--- src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingServiceTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingServiceTest.java
@@ -1,10 +1,15 @@
 package org.cookieandkakao.babting.domain.meeting.service.meetingservice;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import org.cookieandkakao.babting.domain.meeting.dto.request.LocationCreateRequest;
+import org.cookieandkakao.babting.domain.meeting.dto.request.MeetingCreateRequest;
 import org.cookieandkakao.babting.domain.meeting.repository.LocationRepository;
 import org.cookieandkakao.babting.domain.meeting.repository.MeetingRepository;
 import org.cookieandkakao.babting.domain.meeting.repository.MemberMeetingRepository;
 import org.cookieandkakao.babting.domain.meeting.service.MeetingService;
 import org.cookieandkakao.babting.domain.member.service.MemberService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -22,4 +27,15 @@ abstract class MeetingServiceTest {
     protected MemberService memberService;
     @InjectMocks
     protected MeetingService meetingService;
+
+    protected LocationCreateRequest baseLocation;
+    protected MeetingCreateRequest meetingCreateRequest;
+
+    @BeforeEach
+    void setUp(){
+        baseLocation = new LocationCreateRequest("전대", "11", 1.1, 1.1);
+        meetingCreateRequest = new MeetingCreateRequest(baseLocation, "밥팅",
+            LocalDate.now().plusDays(1), LocalDate.now().plusDays(2), 3, LocalTime.of(14, 0),
+            LocalTime.of(17, 0));
+    }
 }
