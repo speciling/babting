67807d8ef9c8ebc714b8df2b830ef9ba41533e38
박민규 <pmq0101@naver.com>
2024-11-12 02:16:25 +0900

    test(MeetingCreateValidationTest): 모임의 모든 정보가 입력되어야하는 테스트 케이스 추가

--- src/test/java/org/cookieandkakao/babting/domain/meeting/controller/validation/MeetingCreateValidationTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/controller/validation/MeetingCreateValidationTest.java
@@ -9,10 +9,16 @@ import jakarta.validation.Validator;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.cookieandkakao.babting.domain.meeting.dto.request.LocationCreateRequest;
 import org.cookieandkakao.babting.domain.meeting.dto.request.MeetingCreateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
 
 class MeetingCreateValidationTest {
 
@@ -72,10 +78,35 @@ class MeetingCreateValidationTest {
             validate.stream().anyMatch(v -> v.getPropertyPath().toString().equals("endDate")));
     }
 
-    @Test
-    void 모임의_모든_정보가_입력되어야한다() {
-        //given
-        //when
-        //then
+    @ParameterizedTest
+    @MethodSource("nullArgument")
+    void 모임의_모든_정보가_입력되어야한다(MeetingCreateRequest request, String fieldName) {
+        // when
+        Set<ConstraintViolation<MeetingCreateRequest>> violations = validator.validate(request);
+
+        // then
+        assertTrue(violations.stream().anyMatch(v -> v.getPropertyPath().toString().equals(fieldName)),
+            fieldName + " 유효성 검사 실패");
+    }
+
+    private static Stream<Arguments> nullArgument() {
+        LocationCreateRequest baseLocation = new LocationCreateRequest("전대", "11", 1.1, 1.1);
+        LocalDate now = LocalDate.now();
+        String title = "밥팅";
+        LocalDate startDate = now.plusDays(1);
+        LocalDate endDate = now.plusDays(2);
+        Integer durationTime = 3;
+        LocalTime startTime = LocalTime.of(14, 0);
+        LocalTime endTime = LocalTime.of(17, 0);
+
+        return Stream.of(
+            Arguments.of(new MeetingCreateRequest(null, title, startDate, endDate, durationTime, startTime, endTime), "baseLocation"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, null, startDate, endDate, durationTime, startTime, endTime), "title"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, title, null, endDate, durationTime, startTime, endTime), "startDate"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, title, startDate, null, durationTime, startTime, endTime), "endDate"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, title, startDate, endDate, null, startTime, endTime), "durationTime"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, title, startDate, endDate, durationTime, null, endTime), "startTime"),
+            Arguments.of(new MeetingCreateRequest(baseLocation, title, startDate, endDate, durationTime, startTime, null), "endTime")
+        );
     }
 }
