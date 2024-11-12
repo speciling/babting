fb49573722713bb8424ce96c63553b5d59d6fbb0
박민규 <pmq0101@naver.com>
2024-11-12 23:47:15 +0900

    test(MeetingExitTest): 주석 추가

--- src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingExitTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingExitTest.java
@@ -11,4 +11,9 @@ class MeetingExitTest extends MeetingServiceTest {
         //when
         //then
     }
+
+    // 모임장이 탈퇴할 경우 모임 삭제
+    // 모임 참여자가 탈퇴할 경우 모임만 탈퇴
+    // 가입하지 않은 모임 탈퇴 불가
+    // 없는 모임 탈퇴 불가
 }
