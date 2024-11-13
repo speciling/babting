a1f681fad150628af9e3e46a5b65f8110f844bc3
박민규 <pmq0101@naver.com>
2024-11-13 21:23:38 +0900

    test(MeetingJoinTest): 모임 참가 테스트 코드 추가

--- src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingJoinTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/meeting/service/meetingservice/MeetingJoinTest.java
@@ -1,14 +1,32 @@
 package org.cookieandkakao.babting.domain.meeting.service.meetingservice;
 
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.cookieandkakao.babting.domain.meeting.entity.Meeting;
+import org.cookieandkakao.babting.domain.meeting.entity.MemberMeeting;
+import org.cookieandkakao.babting.domain.member.entity.Member;
 import org.junit.jupiter.api.Test;
 
 class MeetingJoinTest extends MeetingServiceTest {
 
     @Test
-    void 모임_참가() {
+    void 모임_참가_성공() {
         //given
+        Member joiner = mock(Member.class);
+        Meeting meeting = mock(Meeting.class);
+
+        when(memberService.findMember(1L)).thenReturn(joiner);
+        when(meetingRepository.findById(1L)).thenReturn(Optional.of(meeting));
+
         //when
+        meetingService.joinMeeting(1L, 1L);
+
         //then
+        verify(memberMeetingRepository).save(any(MemberMeeting.class));
     }
 }
