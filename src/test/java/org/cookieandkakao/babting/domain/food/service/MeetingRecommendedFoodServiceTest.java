752bc53557bd1cfc7f37d809d55b72732d7b8a6e
seojiu <seojiu5598@gmail.com>
2024-11-13 17:52:07 +0900

    test(MeetingRecommendedFoodServiceTest): MeetingRecommendedFoodService관련 테스트 생성

--- src/test/java/org/cookieandkakao/babting/domain/food/service/MeetingRecommendedFoodServiceTest.java
+++ src/test/java/org/cookieandkakao/babting/domain/food/service/MeetingRecommendedFoodServiceTest.java
@@ -1,17 +1,19 @@
 package org.cookieandkakao.babting.domain.food.service;
 
+import org.cookieandkakao.babting.domain.food.dto.FoodPreferenceGetResponse;
 import org.cookieandkakao.babting.domain.food.entity.Food;
 import org.cookieandkakao.babting.domain.food.entity.FoodCategory;
 import org.cookieandkakao.babting.domain.food.entity.MeetingNonPreferenceFood;
 import org.cookieandkakao.babting.domain.food.entity.MeetingPreferenceFood;
+import org.cookieandkakao.babting.domain.food.entity.NonPreferenceFood;
 import org.cookieandkakao.babting.domain.food.repository.MeetingNonPreferenceFoodRepository;
 import org.cookieandkakao.babting.domain.food.repository.MeetingPreferenceFoodRepository;
+import org.cookieandkakao.babting.domain.food.repository.NonPreferenceFoodRepository;
 import org.cookieandkakao.babting.domain.meeting.entity.Location;
 import org.cookieandkakao.babting.domain.meeting.entity.Meeting;
 import org.cookieandkakao.babting.domain.meeting.entity.MemberMeeting;
+import org.cookieandkakao.babting.domain.meeting.repository.MemberMeetingRepository;
 import org.cookieandkakao.babting.domain.member.entity.Member;
-import org.cookieandkakao.babting.domain.meeting.service.MeetingService;
-import org.cookieandkakao.babting.domain.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -21,28 +23,26 @@ import org.mockito.MockitoAnnotations;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-class MeetingFoodPreferenceUpdaterTest {
+class MeetingRecommendedFoodServiceTest {
 
     @InjectMocks
-    private MeetingFoodPreferenceUpdater updater;
+    private MeetingRecommendedFoodService recommendedFoodService;
 
+    @Mock
+    private MemberMeetingRepository memberMeetingRepository;
     @Mock
     private MeetingPreferenceFoodRepository meetingPreferenceFoodRepository;
     @Mock
     private MeetingNonPreferenceFoodRepository meetingNonPreferenceFoodRepository;
     @Mock
+    private NonPreferenceFoodRepository nonPreferenceFoodRepository;
+    @Mock
     private FoodRepositoryService foodRepositoryService;
-    @Mock
-    private MemberService memberService;
-    @Mock
-    private MeetingService meetingService;
 
     @BeforeEach
     void setup() {
@@ -50,12 +50,9 @@ class MeetingFoodPreferenceUpdaterTest {
     }
 
     @Test
-    void 모임별선호비선호음식_수정하기_테스트() {
+    void testGetRecommendedFoodDetailsForMeeting() {
         Long memberId = 1L;
         Long meetingId = 1L;
-        List<Long> preferences = List.of(1L, 2L);
-        List<Long> nonPreferences = List.of(3L);
-
         Member member = new Member(memberId);
 
         Location location = new Location("카페", "광주 용봉동", 37.5, 127.0);
@@ -69,27 +66,34 @@ class MeetingFoodPreferenceUpdaterTest {
         Meeting meeting = new Meeting(location, title, startDate, endDate, durationTime, startTime, endTime);
         MemberMeeting memberMeeting = new MemberMeeting(member, meeting, false);
 
-        when(memberService.findMember(memberId)).thenReturn(member);
-        when(meetingService.findMeeting(meetingId)).thenReturn(meeting);
-        when(meetingService.findMemberMeeting(member, meeting)).thenReturn(memberMeeting);
+        when(memberMeetingRepository.findMemberMeetingsByMeetingId(meetingId))
+                .thenReturn(List.of(memberMeeting));
 
         FoodCategory foodCategory1 = new FoodCategory("한식");
         FoodCategory foodCategory2 = new FoodCategory("양식");
-        FoodCategory foodCategory3 = new FoodCategory("일식");
 
         Food food1 = new Food(1L, foodCategory1, "김치찌개");
         Food food2 = new Food(2L, foodCategory2, "스파게티");
-        Food food3 = new Food(3L, foodCategory3, "우동");
 
-        when(foodRepositoryService.findFoodsByIds(any(Set.class)))
-                .thenReturn(List.of(food1, food2, food3));
+        MeetingPreferenceFood preferenceFood = new MeetingPreferenceFood(food1, memberMeeting);
+        MeetingNonPreferenceFood nonPreferenceFood = new MeetingNonPreferenceFood(food2, memberMeeting);
 
-        updater.updatePreferences(meetingId, memberId, preferences, nonPreferences);
+        when(meetingPreferenceFoodRepository.findAllByMemberMeeting(memberMeeting))
+                .thenReturn(List.of(preferenceFood));
+        when(meetingNonPreferenceFoodRepository.findAllByMemberMeeting(memberMeeting))
+                .thenReturn(List.of(nonPreferenceFood));
 
-        verify(meetingPreferenceFoodRepository, times(1)).deleteAllByMemberMeeting(memberMeeting);
-        verify(meetingNonPreferenceFoodRepository, times(1)).deleteAllByMemberMeeting(memberMeeting);
+        NonPreferenceFood personalNonPreferenceFood = new NonPreferenceFood(food2, memberMeeting.getMember());
+        when(nonPreferenceFoodRepository.findAllByMember(any())).thenReturn(List.of(personalNonPreferenceFood));
 
-        verify(meetingPreferenceFoodRepository, times(1)).saveAll(any(List.class));
-        verify(meetingNonPreferenceFoodRepository, times(1)).saveAll(any(List.class));
+        when(foodRepositoryService.findFoodsByIds(Set.of(1L)))
+                .thenReturn(List.of(food1));
+
+        List<FoodPreferenceGetResponse> responses = recommendedFoodService.getRecommendedFoodDetailsForMeeting(meetingId);
+
+        assertEquals(1, responses.size());
+        assertEquals("김치찌개", responses.get(0).name());
+        assertEquals("한식", responses.get(0).category());
+        assertEquals(1L, responses.get(0).foodId());
     }
 }
