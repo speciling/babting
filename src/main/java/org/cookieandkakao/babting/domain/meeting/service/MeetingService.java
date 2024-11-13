947b273c16d1c1d820223507c59fc3ec099f5e50
박민규 <pmq0101@naver.com>
2024-11-13 21:47:10 +0900

    remove(MeetingService): 주석 제거

--- src/main/java/org/cookieandkakao/babting/domain/meeting/service/MeetingService.java
+++ src/main/java/org/cookieandkakao/babting/domain/meeting/service/MeetingService.java
@@ -42,7 +42,6 @@ public class MeetingService {
         this.memberService = memberService;
     }
 
-    // 모임 생성(주최자)
     public void createMeeting(Long memberId, MeetingCreateRequest meetingCreateRequest){
         Member member = memberService.findMember(memberId);
         Meeting meeting = meetingCreateRequest.toEntity();
@@ -52,7 +51,6 @@ public class MeetingService {
         memberMeetingRepository.save(new MemberMeeting(member, meeting, true));
     }
 
-    // 모임 수정
     public void updateMeeting(Long memberId, Long meetingId, MeetingUpdateRequest meetingUpdateRequest) {
         Member member = memberService.findMember(memberId);
         Meeting meeting = findMeeting(meetingId);
@@ -73,7 +71,6 @@ public class MeetingService {
         meeting.updateEndTime(meetingUpdateRequest.endTime());
     }
 
-    // 모임 참가(초대받은사람)
     public void joinMeeting(Long memberId, Long meetingId){
         Member member = memberService.findMember(memberId);
         Meeting meeting = findMeeting(meetingId);
@@ -86,7 +83,6 @@ public class MeetingService {
         memberMeetingRepository.save(new MemberMeeting(member, meeting, false));
     }
 
-    // 모임 탈퇴(주최자, 초대받은 사람)
     public void exitMeeting(Long memberId, Long meetingId){
         Member member = memberService.findMember(memberId);
         Meeting meeting = findMeeting(meetingId);
@@ -103,7 +99,6 @@ public class MeetingService {
         }
     }
 
-    // 내가 참여한 모임 목록 조회
     public List<MeetingGetResponse> getAllMeetings(Long memberId){
         Member member = memberService.findMember(memberId);
         List<Meeting> meetingList = memberMeetingRepository.findMeetingsByMember(member);
@@ -112,19 +107,16 @@ public class MeetingService {
             .toList();
     }
 
-    // 모임 이름 조회, 시작과 끝 날짜 조회
     public MeetingInfoGetResponse getMeetingInfo(Long meetingId){
         Meeting meeting = findMeeting(meetingId);
         return MeetingInfoGetResponse.from(meeting);
     }
 
-    // 모임 확정 날짜, 확정 음식 확인
     public MeetingConfirmedInfoResponse getMeetingConfirmedInfo(Long meetingId){
         Meeting meeting = findMeeting(meetingId);
         return MeetingConfirmedInfoResponse.from(meeting);
     }
 
-    // 주최자 확인
     public MeetingHostCheckResponse checkHost(Long memberId, Long meetingId){
         Member member = memberService.findMember(memberId);
         Meeting meeting = findMeeting(meetingId);
